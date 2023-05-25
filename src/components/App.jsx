d210c641392bc9294917473db4886a6ce0b0af01
Bad-Raider <stanislav.gusiev@gmail.com>
2023-05-25 21:52:09 +0300

    react dom

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+    <div>
+      <Routes>
+        <Route></Route>
+        <Route></Route>
+        <Route></Route>
+      </Routes>
       React homework template
     </div>
   );
 };
+
+export default App;
